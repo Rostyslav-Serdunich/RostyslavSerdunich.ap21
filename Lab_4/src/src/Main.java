8281f8bd5c4999546cfd88ad798cbdc70a105412
AdmiN <rserdunic@gmail.com>
2026-04-18 20:16:18 +0300

    Lab_4

---
+++ Lab_4/src/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("=== Лабораторна робота №4 ===");
+
+
+        // Виклик стандартних методів main з кожного класу
+        Task1.main(args);
+        Task2.main(args);
+        Task3.main(args);
+        Task4.main(args);
+
+    }
+}
